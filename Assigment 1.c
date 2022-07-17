48933de84323286ff58f89bc0be1374175780936
AjeetkuSingh <105101130+AjeetkuSingh@users.noreply.github.com>
2022-07-18 00:21:52 +0530

    Add files via upload

---
+++ Assigment 1.c
@@ -0,0 +1,132 @@
+//sol 1//
+/*
+#include<stdio.h>
+int main()
+{
+printf("Hello Students");
+return 0;
+}
+*/
+
+//Sol 2//
+/*
+#include<stdio.h>
+int main()
+{
+    printf("Hello\nStudents");
+    return 0;
+}
+*/
+
+//Sol 3//
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"MYSIRG\"");
+    return 0;
+}
+*/
+
+//Sol 4//
+/*
+#include<stdio.h>
+int main()
+{
+    int r;
+    float a;
+    printf("Enter radius of circlr");
+    scanf("%d",&r);
+    a=3.14*r*r;
+    printf("Area is %f",a);
+    printf("\n");
+    return 0;
+}
+*/
+
+//Sol 5//
+/*
+#include<stdio.h>
+int main()
+{
+    int x;
+    x=printf("ABC");
+    printf("Length of \"ABC\" is %d",x);
+    printf("\n");
+    return 0;
+}
+*/
+//Sol 6//
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\"Hello, Amit kumar\"");
+    return 0;
+}
+*/
+//Sol 7//
+/*
+#include<stdio.h>
+int main()
+{
+    printf("%%d");
+    printf("\n");
+    return 0;
+}
+*/
+//Sol 8//
+/*
+#include<Stdio.h>
+int main()
+{
+    printf("\\n");
+    return 0;
+}
+*/
+//Sol 9//
+/*
+#include<stdio.h>
+int main()
+{
+    printf("\\\\");
+    return 0;
+}
+*/
+//Sol 10//
+/*
+#include<stdio.h>
+int main()
+{
+    int day, month, year;
+    Printf("Enter date in DD/MM/YYYY format\n");
+    scanf("%d/%d/%d", &day,&month,&year);
+    printf("day-%d,month-%d,year-%d",day,month,year);
+    return 0;
+}
+*/
+//Sol 11//
+/*
+#include<stdio.h>
+int main()
+{
+    int hour,min;
+    printf("Enter time in HH:MM format\n");
+    scanf("%d:%d",&hour,&min);
+    printf("%d hours and %d minutes",hour,min);
+    return 0;
+}
+*/
+//Sol 12//
+#include<stdio.h>
+int main()
+{
+    int x=printf("ineuron");
+    printf("%d",x);
+    return 0;
+}
+
+
+
+
+
